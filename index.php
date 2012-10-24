8da5a3544e5af4015a2ecea20c1320140eb4af9d
Jarvis Stubblefield <jarvis.stubblefield@bytesofknowledge.com>
2012-10-24 01:04:15 -0500

    Added index page just for ease of setting up apache and getting to the challenges output.

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Coding Challenge Home</title>
+    </head>
+    <body>
+        <ol>
+            <li><a href="/scrape2012Olympics.php">2012 Medals Scrape and Graph</a></li>
+            <li><a href="/redditSubmission.php">Reddit Ajax Submissions</a></li>
+            <li><a href="/fizzbuzz.php">FizzBuzz Fewest Bytes</a></li>
+        </ol>
+    </body>
+</html>
